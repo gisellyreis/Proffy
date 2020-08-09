3e8319c3b146c5ce70f48556630b7a30d1fbeeac
gisellyreis <gisellyareis@gmail.com>
2020-08-08 22:35:31 -0300

    web da aplicação Adiciona páginas/funcionalidades na aplicação web. Faz conexão do front-end com a api/back-end. Finaliza versão 1.0 da aplicação web.

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,33 +3,51 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+    avatar: string;
+    bio: string;
+    cost: number;
+    id: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+
+    function createConnection() {
+        api.post('connections', {
+            user_id: teacher.id,
+        })
+    }
+
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars2.githubusercontent.com/u/47332912?s=460&v=4" alt="Giselly Reis" />
+                <img src={teacher.avatar} alt={teacher.name} />
                 <div>
-                    <strong>Giselly Reis</strong>
-                    <span>Programação</span>
+                    <strong> {teacher.name} </strong>
+                    <span> {teacher.subject} </span>
                 </div>
             </header>
 
-            <p>
-                As vezes não sei nem onde eu tô, mas consigo me localizar facilmente em qualquer lugar. Tenho memória fotográfica e nunca fico perdido.
-                <br/><br/>
-                Eu ensino a galera como não se perder na vida, com lições geográficas simples pra você nunca mais precisar de mapa na sua bela vida.
-            </p>
+            <p> {teacher.bio} </p>
 
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$ 30,00</strong>
+                    <strong>R$ {teacher.cost} </strong>
                 </p>
-                <button type="button" >
+                <a target="_blank" onClick={createConnection} href={`https://wa.me/${teacher.whatsapp}`} >
                     <img src={whatsappIcon} alt="Whatsapp"/>
                     Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
     );
